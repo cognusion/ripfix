661b5705860caf2dc3172323fe076a3f230799a3
M <m@cognusion.com>
2025-06-11 22:15:37 -0400

    Adds existing code.

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/cognusion/ripfix
+
+go 1.23
+
+require (
+	github.com/cognusion/go-sequence v1.1.0
+	github.com/cognusion/semaphore v1.3.2
+	github.com/spf13/pflag v1.0.6
+)
+
+require github.com/speps/go-hashids/v2 v2.0.1 // indirect
---
+++ rip.go
@@ -0,0 +1,287 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/cognusion/go-sequence"
+	"github.com/cognusion/semaphore"
+	"github.com/spf13/pflag"
+)
+
+var (
+	pdfs      []string
+	maxP      int
+	out       string
+	tmp       string
+	tmpFolder = "ripfix"
+	clean     bool
+)
+
+// work gets passed around to various funcs/goros.
+type work struct {
+	id    string
+	pdf   string
+	tmp   string
+	out   string
+	clean bool
+}
+
+func init() {
+	pflag.StringSliceVarP(&pdfs, "pdfs", "p", make([]string, 0), "List of PDFs to convert. Globs are fine. Quotes are encouraged.")
+	pflag.StringVarP(&out, "out", "o", "./", "Location to place the final products. They will have the same file name as the source.")
+	pflag.StringVarP(&tmp, "temp", "t", os.TempDir()+"/", "Location for temp files.")
+	pflag.IntVarP(&maxP, "max", "m", runtime.NumCPU(), "Maximum number of simultaneous processors.")
+	pflag.BoolVar(&clean, "clean", true, "Remove temp folders/files when complete.")
+
+	pflag.Parse()
+
+	if len(pdfs) == 0 {
+		fmt.Println("ripfix options:")
+		pflag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	// Sanity!
+	if maxP < 1 {
+		// We need at least one processor, or deadlock
+		maxP = 1
+	}
+	if !strings.HasSuffix(tmp, "/") {
+		tmp += "/"
+	}
+	if !strings.HasSuffix(out, "/") {
+		out += "/"
+	}
+}
+
+func main() {
+
+	var (
+		pid      = os.Getpid()
+		seq      = sequence.New(1)
+		sem      = semaphore.NewSemaphore(maxP)
+		workChan = make(chan work)
+	)
+
+	// Step -1 Check and set
+
+	// Check for pdftoppm and tesseract
+	if _, err := exec.LookPath("pdftoppm"); err != nil {
+		fmt.Println("Could not find path to pdftoppm!")
+		os.Exit(1)
+	}
+	if _, err := exec.LookPath("tesseract"); err != nil {
+		fmt.Println("Could not find path to tesseract!")
+		os.Exit(1)
+	}
+
+	// Confirm out is a folder
+	if s, serr := os.Stat(out); serr != nil {
+		panic(serr)
+	} else if !s.IsDir() {
+		fmt.Printf("Output location '%s' is not a directory.\n", out)
+		os.Exit(1)
+	}
+
+	// Ensure the base tmp folder is available
+	if terr := os.MkdirAll(tmp+tmpFolder, os.ModePerm); terr != nil {
+		panic(terr)
+	}
+	if clean {
+		defer os.RemoveAll(tmp + tmpFolder)
+	}
+
+	// Step 0 workers
+	// We are ok with starting too many workers, as any unneeded will just exit
+	// after the work is assigned.
+	progressChan, doneChan := supervisor(&sem, workChan)
+	defer close(progressChan)
+
+	// read from progressChan and print stuff
+	go func() {
+		for p := range progressChan {
+			switch v := p.(type) {
+			case error:
+				fmt.Printf("[PROGRESS] ERROR: %s\n", v)
+				os.Exit(1)
+			case string:
+				fmt.Printf("[PROGRESS] %s\n", v)
+			default:
+				fmt.Printf("[PROGRESS] ??: %+v\n", v)
+			}
+		}
+	}()
+
+	// Step 1 build work and dole it out
+	for _, file := range buildList(pdfs) {
+		id := seq.NextHashID()
+		//fmt.Printf("[WORKFILE] %s is %s\n", file, id)
+		workChan <- work{
+			id:    id,
+			pdf:   file,
+			tmp:   fmt.Sprintf("%s%s/%d.%s/", tmp, tmpFolder, pid, id),
+			out:   out,
+			clean: clean,
+		}
+	}
+	// POST: each work{} has been consumed by a worker.
+
+	// Signal we're done, so any idle workers can exit
+	close(doneChan)
+
+	// wait until all of the workers are done
+	<-sem.IsFree(100 * time.Millisecond)
+}
+
+// supervisor takes a Semaphore and a channel where work will be assigned, immediately returning two channels:
+//
+//	The first channel (progressChan) is where progress updates (string) an errors (error) are sent. If an error is sent, the worker will exit.
+//	The second channel (doneChan) is used to signal the supervisor and workers when there will be no more work assigned, via closing.
+//
+// The supervisor ensures that there are always 'maxP' workers waiting (using the Semaphore), listening for work, until doneChan is closed.
+// Each worker does *at most one* item of work, exiting when it is complete. The supervisor will start a new worker if it is still employed.
+func supervisor(lock *semaphore.Semaphore, workChan chan work) (progressChan chan any, doneChan chan struct{}) {
+	doneChan = make(chan struct{})
+	progressChan = make(chan any)
+
+	go func() {
+		// TODO: Get rid of this incrementer. But good for debugging.
+		c := 0
+		for {
+			c++
+			select {
+			case <-lock.Until():
+				// woo! make a worker!
+				go func(i int) {
+					progressChan <- fmt.Sprintf("[WORKER %d] Start", i)
+					defer lock.Unlock()
+					defer func() { progressChan <- fmt.Sprintf("[WORKER %d] Done", i) }()
+
+					select {
+					case w := <-workChan:
+						var err error
+						// Step 2 get work
+						progressChan <- fmt.Sprintf("[WORKER %d] Work! %+v", i, w)
+
+						// Step 3 ensure path
+						err = os.MkdirAll(w.tmp, os.ModePerm)
+						if err != nil {
+							progressChan <- fmt.Errorf("[WORKER %d] Error MkdirAll '%s': %w", i, w.tmp, err)
+							return
+						}
+						if w.clean {
+							// These temp folder full of TIFFs can get massive.
+							// Clean up.
+							defer os.RemoveAll(w.tmp)
+						}
+
+						// Step 4a explode PDFs into TIFFs
+						progressChan <- fmt.Sprintf("[WORKER %d] pdfToTiff(%s, %s)", i, w.pdf, w.tmp)
+						err = pdfToTiff(w.pdf, w.tmp)
+						if err != nil {
+							progressChan <- fmt.Errorf("[WORKER %d] Error pdftoppm '%s' -> '%s': %w", i, w.pdf, w.tmp, err)
+							return
+						}
+						// Step 4b create list of result files, w.tmp+w.id+".lst"
+						progressChan <- fmt.Sprintf("[WORKER %d] createTiffList", i)
+						listFile, lErr := createTiffList(&w)
+						if lErr != nil {
+							progressChan <- fmt.Errorf("[WORKER %d] Error createTiffList: %w", i, lErr)
+							return
+						}
+
+						// Step 5 tesseract the TIFFs
+						outFile := w.out + strings.TrimSuffix(filepath.Base(w.pdf), filepath.Ext(filepath.Base(w.pdf))) + "_fixed" // tesseract wants an extensionless filename
+						progressChan <- fmt.Sprintf("[WORKER %d] tesseract(%s, %s)", i, listFile, outFile)
+						err = tesseract(listFile, outFile)
+						if err != nil {
+							progressChan <- fmt.Errorf("[WORKER %d] Error tesseract '%s' -> '%s': %w", i, w.tmp, outFile, err)
+							return
+						}
+
+						// Step 5 celebrate!
+						progressChan <- fmt.Sprintf("[WORKER %d] Completed Work! See '%s.pdf'", i, outFile)
+					case <-doneChan:
+						// That's all folks!
+						return
+					}
+				}(c)
+			case <-doneChan:
+				// That's all folks!
+				return
+			}
+		}
+	}()
+
+	return // progressChan, doneChan
+}
+
+// buildList will possibly recursively (if a glob is provided) create a list of files to assign as work.
+func buildList(files []string) []string {
+	l := make([]string, 0)
+	for _, file := range files {
+		//fmt.Printf("[FILE] %s\n", file)
+		if strings.Contains(file, "*") {
+			gfiles, err := filepath.Glob(file)
+			if err != nil {
+				panic(err)
+			}
+			l = append(l, buildList(gfiles)...) // recursion
+		} else if s, err := os.Stat(file); err != nil {
+			// We we can't stat the thing, something is very wrong.
+			panic(err)
+		} else if !s.IsDir() {
+			// file
+			l = append(l, file)
+		}
+	}
+	return l
+}
+
+// simplerun is an abstraction to execute a Command.
+func simpleRun(name string, args ...string) error {
+	cmd := exec.Command(name, args...)
+	return cmd.Run()
+}
+
+// pdfToTiff constructs a pdftoppm Command to extract PDF pages as TIFF images.
+func pdfToTiff(pdf string, outFolder string) error {
+	return simpleRun("pdftoppm", "-tiff", "-r", "300", pdf, outFolder+"page")
+}
+
+// createTiffList assembles a list of -presumably- the TIFF images created by pdfToTiff,
+// writing it to a file that tesseract can read.
+func createTiffList(w *work) (string, error) {
+	var (
+		gfiles []string
+		f      *os.File
+		err    error
+	)
+	listFile := w.tmp + w.id + ".lst"
+	gfiles, err = filepath.Glob(w.tmp + "*.tif")
+	if err != nil {
+		return "", fmt.Errorf("error getting tiffs '%s': %w", w.tmp, err)
+	}
+	f, err = os.Create(listFile)
+	if err != nil {
+		return "", fmt.Errorf("error creating list file '%s': %w", listFile, err)
+	}
+	defer f.Close()
+	for _, line := range gfiles {
+		if _, werr := f.WriteString(line + "\n"); werr != nil {
+			return "", fmt.Errorf("error writing to '%s': %w", listFile, err)
+		}
+	}
+	return listFile, nil
+}
+
+// tesseract constructs a tesseract Command to do OCR on the TIFF images and reassemble them as a PDF.
+func tesseract(fileList, outpath string) error {
+	return simpleRun("tesseract", fileList, outpath, "pdf")
+}
